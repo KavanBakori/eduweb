4602a25283bf6adee74f823f4b59a1f40f931f7e
KavanBakori <kavanbakori111@gmail.com>
2024-02-07 18:08:49 +0530

    kavancommit

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import State from '../../Component/State/State';
+import Footer from '../../Component/Footer/Footer';
+
+
+
+function Home ()  {
+  return (
+    <>
+    <State/>
+    <Footer/>
+    </>
+  )
+}
+
+export default Home;
