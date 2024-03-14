c4c28c44c9591cb5b138747e65276990c2cd950a
sakshi-ajagiya <sakshiajagiya3@gmail.com>
2024-03-14 16:56:45 +0530

    xyz

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css'
 import Home from  './Pages/Home/Home';
 import Login from './Pages/Login/Login';
+import Index from './Component/temp/Index';
 
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path='/' element={<Home />} />
           <Route path='/login' element={<Login />} />
+          <Route path='/Index' element={<Index />} />
         </Routes>
       </Router>
     </div>
